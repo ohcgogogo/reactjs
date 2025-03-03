c90ceb2f96eb765a01f1b11c69d2ed380c7ef679
ohcgogogo <ohcgogogo@gmail.com>
2025-03-03 14:47:08 +0900

    package.json 설정

---
+++ README.md
@@ -0,0 +1,9 @@
+## Description
+React & Typescript 프로젝트 구성 샘플
+
+## Project setup
+
+```bash
+$ npm install
+```
+
--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -4,7 +4,7 @@ import logger from 'redux-logger';
 
 const store = configureStore({
     reducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger as any),
 });
 
 export type AppDispatch = typeof store.dispatch
